79fc58009370964cb32af6ee972334d9d0c1f0fd
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-09-09 16:53:45 +0100

    Added chatgpt-3.5-turbo assistant for specific channel

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Discord from 'discord.js'
+import { OpenAI } from 'openai'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -8,15 +9,38 @@ const client = new Discord.Client({
     ]
 })
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI
+})
+
 client.on('ready', () => {
   console.log(`The bot ${client.user?.tag} is ready!`)
 })
 
-client.on('messageCreate', (message) => {
+const prefix = '!'
+const channels = [process.env.CHANNEL]
+
+client.on('messageCreate', async (message) => {
+
   if (message.author.bot) return
-  message.reply({
-    content: 'im alive!'
+  if (message.content.startsWith(prefix)) return
+  if (!channels.includes(message.channelId)) return
+  const res = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: 'chatgpt is a friendly chatbot.'
+      },
+      {
+        role: 'user',
+        content: message.content
+      }
+    ]
+  }).catch(err => {
+    console.error('OpenAI Error: ', err)
   })
+  message.reply(res.choices[0].message.content)
 })
 
 client.login(process.env.TOKEN)
